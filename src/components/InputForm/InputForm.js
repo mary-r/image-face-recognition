ad85365475121fee5c92ff443c9a1ab8c90947a5
Mary Romeral <maryromeral@Marys-MacBook-Pro.local>
2020-05-08 13:50:07 -0400

    fixed form layouts

--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -8,8 +8,8 @@ const InputForm = ({ onInputChange, onButtonClick }) => {
                 {'This app will detect faces in your pictures. Give it a try.'}
             </p>
             <div>
-                <article className='form br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 v-mid center'>
-                    <div className='pa4 black-80'>
+                <article className='form br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw8 shadow-5 center'>
+                    <div className='pa4'>
                         <input className='input-reset ba b--black-20 pa2 mb2 db w-100 input' type='text' onChange={onInputChange} />
                         <button className='f6 link grow ph3 pv2 mb2 dib detect' style={{textTransform: 'lowercase'}} onClick={onButtonClick}>Detect</button>
                     </div>
