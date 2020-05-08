ada22ee22e90d987cf9b9aa2fac327fd6ab2c9da
Mary Romeral <maryromeral@Marys-MacBook-Pro.local>
2020-05-08 13:42:33 -0400

    fixed input form

--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -9,8 +9,10 @@ const InputForm = ({ onInputChange, onButtonClick }) => {
             </p>
             <div>
                 <article className='form br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 v-mid center'>
-                    <input className='input-reset ba b--black-20 pa2 mb2 db w-100 input' type='text' onChange={onInputChange} />
-                    <button className='f6 link grow ph3 pv2 mb2 dib detect' style={{textTransform: 'lowercase'}} onClick={onButtonClick}>Detect</button>
+                    <div className='pa4 black-80'>
+                        <input className='input-reset ba b--black-20 pa2 mb2 db w-100 input' type='text' onChange={onInputChange} />
+                        <button className='f6 link grow ph3 pv2 mb2 dib detect' style={{textTransform: 'lowercase'}} onClick={onButtonClick}>Detect</button>
+                    </div>
                 </article>
             </div>
         </div>
