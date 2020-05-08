76f544ad13dde5289cb677ba46569d73a8fe5d8e
Mary Romeral <maryromeral@Marys-MacBook-Pro.local>
2020-05-08 12:03:05 -0400

    deploying

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ class App extends Component {
   // detect btn
   onButtonClick = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageUrl', {
+      fetch('https://infinite-waters-16481.herokuapp.com/imageUrl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -105,7 +105,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://infinite-waters-16481.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
